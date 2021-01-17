c2297b461ae405f340aafab1b6ec85943512a334
claudionunezjr <claudio.nunez.jr@live.com>
2021-01-17 14:57:36 -0800

    Hard-coded app content for activity.

--- src/activities/app-content.ts
+++ src/activities/app-content.ts
@@ -27,28 +27,4 @@ ${contentFields}
 
 export const AppContentActivity = Activity<
     AppContentActivityProps | undefined
->();
-
-fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${space}/environments/${environment}`,
-    {
-        body: JSON.stringify({
-            query,
-            variables: {
-                isPreview
-            }
-        }),
-        headers: {
-            Authorization: `Bearer ${contentfulToken}`,
-            'Content-Type': 'application/json'
-        },
-        method: 'POST'
-    }
-)
-    .then((res) => res.json())
-    .then(({ data }) => {
-        console.log('data', data);
-        AppContentActivity.update({
-            site: data.siteCollection?.items[0]
-        });
-    });
+>(JSON.parse('{"pageCollection":{"items":[{"title":"Home","slug":"/","contentCollection":{"items":[{"title":"Get Started","description":{"json":{"data":{},"content":[{"data":{},"content":[{"data":{},"marks":[],"value":"A lightweight, functional JavaScript framework for building component-based reactive applications.","nodeType":"text"}],"nodeType":"paragraph"}],"nodeType":"document"}},"contentCollection":{"items":[]}}]}}]},"siteCollection":{"items":[{"icons":null,"logo":{"contentType":"image/png","description":"Site logo.","height":91,"url":"https://images.ctfassets.net/2x238mu87414/6XeR7Z8onDfOTi9GK7pyTr/000f41ee0d845efdd3bf796491dba73f/nectar-logo-v1.png","width":86},"pagesCollection":{"items":[{"title":"Docs","slug":"docs"},{"title":"API","slug":"api"},{"title":"Guides","slug":"guides"}]},"seoDescription":"Nectar JS is a lightweight, functional JavaScript framework for building component-based reactive applications.","shortDescription":"A lightweight, functional JavaScript framework for building component-based reactive applications.","title":"Nectar"}]}}'));
