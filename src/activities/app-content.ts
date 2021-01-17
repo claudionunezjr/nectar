c370ecd383dbcffe19e2963b655e1fdfc5a53e27
claudionunezjr <claudio.nunez.jr@live.com>
2021-01-17 14:59:00 -0800

    Removed unused imports.

--- src/activities/app-content.ts
+++ src/activities/app-content.ts
@@ -1,29 +1,10 @@
 import { Activity } from '@darkkenergy/nectar';
 
 import { Site } from '@app/types';
-import {
-    assetFields,
-    contentFields,
-    pagesCollection,
-    siteCollection
-} from '@app/contentful/graphql';
 
 export interface AppContentActivityProps {
     site: Site;
 }
-
-const contentfulToken = '4CTufSIoA8Raqrc-0c-7ANq-ch4vHWgzXAwH3cv7uOI';
-const environment = 'master';
-const space = '2x238mu87414';
-const isPreview = true;
-const query = `
-query ($isPreview: Boolean = true) {
-    ${pagesCollection}
-    ${siteCollection}
-}
-${assetFields}
-${contentFields}
-`;
 
 export const AppContentActivity = Activity<
     AppContentActivityProps | undefined
